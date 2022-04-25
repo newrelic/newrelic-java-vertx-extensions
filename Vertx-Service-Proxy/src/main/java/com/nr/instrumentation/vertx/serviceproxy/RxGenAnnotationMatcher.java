035fbf7a159e366b841b1fb7c827da8c3f1af9e7
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2022-04-25 06:45:39 -0500

    baseline 4-25

---
+++ Vertx-Service-Proxy/src/main/java/com/nr/instrumentation/vertx/serviceproxy/RxGenAnnotationMatcher.java
@@ -0,0 +1,13 @@
+package com.nr.instrumentation.vertx.serviceproxy;
+
+import com.newrelic.agent.instrumentation.annotationmatchers.AnnotationMatcher;
+
+public class RxGenAnnotationMatcher implements AnnotationMatcher {
+
+	@Override
+	public boolean matches(String annotationDesc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
